e2c077d07041dac5d8eab3f8fd6380725e8236fb
WhoamIthinking <1185877689@qq.com>
2025-01-03 18:41:43 +0800

    添加生成子集的功能及其排序实现，并更新相关文档

--- test_127.c
+++ test_127.c
@@ -1,73 +1,107 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-// 打印子集
-void printSubset(int *subset, int subsetSize) {
-    for (int i = 0; i < subsetSize; i++) {
-        if (i > 0) printf(" ");
-        printf("%d", subset[i]);
+int a[5000][12];
+
+// 生成子集
+void generate(int n, int b[]) {
+    int i = 0;
+    int j;
+    for (i = 0; i < pow(2, n); i++) {
+        for (j = 0; j < n; j++) {
+            if (i & (1 << j)) {
+                a[i][j] = b[j];
+            } else {
+                a[i][j] = 0; // 确保未选中的元素为0
+            }
+        }
     }
-    printf("\n");
 }
 
 // 比较函数，用于qsort排序
-int compare(const void *a, const void *b) {
-    int *arrA = *(int **)a;
-    int *arrB = *(int **)b;
-    int sizeA = arrA[0];
-    int sizeB = arrB[0];
+int compare(const void *p1, const void *p2) {
+    int *arrA = (int *)p1;
+    int *arrB = (int *)p2;
+    int sizeA = 0, sizeB = 0;
+
+    // 计算子集的大小
+    for (int i = 0; i < 12; i++) {
+        if (arrA[i] != 0) sizeA++;
+        if (arrB[i] != 0) sizeB++;
+    }
 
     // 按子集大小排序
     if (sizeA != sizeB) return sizeA - sizeB;
 
     // 如果大小相同，按字典序排序
-    for (int i = 1; i <= sizeA && i <= sizeB; i++) {
-        if (arrA[i] != arrB[i]) return arrA[i] - arrB[i];
+    for (int i = 0; i < 12; i++) {
+        if (arrA[i] != arrB[i]) return arrB[i] - arrA[i];
     }
 
     return 0;
 }
 
+// 排序函数
+void mysort(int n) {
+    qsort(a, (int)pow(2, n), sizeof(a[0]), compare);
+}
+
 int main() {
     int n;
-    // 读取输入的整数n
     scanf("%d", &n);
+    int i = 0, j = 0;
 
-    // 动态分配数组arr，存储1到n的整数
-    int *arr = (int *)malloc(n * sizeof(int));
-    for (int i = 0; i < n; i++) {
-        arr[i] = i + 1;
+    // 初始化数组a
+    for (i = 0; i < pow(2, n); i++) {
+        for (j = 0; j < n; j++) {
+            a[i][j] = 0;
+        }
     }
 
-    // 计算所有子集的总数
-    int totalSubsets = 1 << n;
-    // 动态分配二维数组subsets，存储所有子集
-    int **subsets = (int **)malloc(totalSubsets * sizeof(int *));
-    int subsetIndex = 0;
+    // 读取输入的n个整数
+    int *b = (int *)malloc(sizeof(int) * n);
+    for (j = 0; j < n; j++) {
+        scanf("%d", &b[j]);
+    }
 
-    // 生成所有子集
-    for (int i = 0; i < totalSubsets; i++) {
-        int *subset = (int *)malloc((n + 1) * sizeof(int));
-        subset[0] = 0; // 子集的大小存储在第一个元素
-        for (int j = 0; j < n; j++) {
-            if (i & (1 << j)) {
-                subset[++subset[0]] = arr[j];
+    // 生成子集
+    generate(n, b);
+    // 排序子集
+    mysort(n);
+
+    // 打印不重复的子集
+    int *last = NULL;
+    for (i = 0; i < pow(2, n); i++) {
+        int *c=(int *)malloc(sizeof(int)*n);
+        for(j=0;j<n;j++){
+            c[j]=0;
+        }
+        int k=0;
+        int first = 1; // 标记是否是第一个元素
+        for (j = 0; j < n; j++) {
+            if (a[i][j] != 0) {
+                c[k]=a[i][j];
+                k++;
             }
         }
-        subsets[subsetIndex++] = subset;
+        if(last!=NULL&&compare(c,last)==0){
+            last=c;
+            continue;
+        }
+        for(j=0;j<k;j++){
+            if(j==0){
+                printf("%d",c[j]);
+            }
+            else{
+                printf(" %d",c[j]);
+            }
+        }
+        free(last);
+        last = c;
+        printf("\n");
     }
 
-    // 对所有子集进行排序
-    qsort(subsets, totalSubsets, sizeof(int *), compare);
-
-    // 打印所有子集并释放内存
-    for (int i = 0; i < totalSubsets; i++) {
-        printSubset(subsets[i] + 1, subsets[i][0]);
-        free(subsets[i]);
-    }
-
-    free(subsets);
-    free(arr);
-
+    free(b);
     return 0;
 }
